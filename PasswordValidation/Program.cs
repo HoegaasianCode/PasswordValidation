a1571411d0d6f6cc06c408020a02ebb30a68006d
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-29 14:30:21 +0200

    Initial commit

---
+++ PasswordValidation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PasswordValidation
+{
+    class Program
+    {
+        // https://edabit.com/challenge/etT7orqDDXJF2zGYM
+        // UNFINISHED
+
+        static void Main(string[] args)
+        {
+            Validator password = new("P1zz@");
+        }
+    }
+}
---
+++ PasswordValidation/Validator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordValidation
+{
+    public class Validator
+    {
+        private readonly string _password;
+        private string ShiftedPassword { get; set; }
+        private string ShiftedPassword1 { get; set; }
+        private bool IsValid { get; set; }
+
+        public Validator(string password) // "P1zz@"
+        {
+            _password = password;
+            IsValid = true;
+        }
+
+        public void IsCorrectLength()
+        {
+            if (_password.Length > 24 ||
+                _password.Length < 6) IsValid = false;
+        }
+
+        public void HasCorrectCases()
+        {
+            bool isUpper = false;
+            bool isLower = false;
+            for(int i = 0; i < _password.Length; i++)
+            {
+                char c = _password[i];
+                if(Char.IsUpper(c)) isUpper = true;
+                if(Char.IsLower(c)) isLower = true;
+            }
+            if (!isUpper || !isLower) IsValid = false;
+        }
+
+        public void IsDigit()
+        {
+            bool isDigit = false;
+            for(int i = 0; i < _password.Length; i++)
+            {
+                char c = _password[i];
+                if (Char.IsNumber(c)) isDigit = true;
+            }
+            if (!isDigit) IsValid = false;
+        }
+
+        public void HasRepeatingCharacters()
+        {
+            bool isRepeating = false;
+            for(int i = 0; i < _password.Length; i++)
+            {
+                char c = _password[i];
+                char c1 = ShiftedPassword[i];
+                char c2 = ShiftedPassword1[i];
+                if (c == c1 && c1 == c2) isRepeating = true;
+            }
+            if (!isRepeating) IsValid = false;
+        }
+
+        public void CreateShiftedString1() // "aaaW123@"
+        {
+            string s = "";
+            for(int i = 1; i < _password.Length; i++)
+            {
+                char c = _password[i];
+                s += c;
+            }
+            ShiftedPassword = s;
+        }
+
+        public void CreateShiftedString2() // "W123aaa@"
+        {
+            string s = "";
+            for (int i = 2; i < _password.Length; i++)
+            {
+                char c = _password[i];
+                s += c;
+            }
+            ShiftedPassword1 = s;
+        }
+    }
+}

9cc9225d4fe57cb1a3ca3bebbda97efb975fe9b2
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-03-30 18:38:42 +0100

    Update Program.cs

--- PasswordValidation/Program.cs
+++ PasswordValidation/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
+using System;
 
 namespace PasswordValidation
 {
     class Program
     {
         // https://edabit.com/challenge/etT7orqDDXJF2zGYM
-        // UNFINISHED
+        // SOLVED
 
         static void Main(string[] args)
         {
-            Validator password = new("P1zz@");
+            Validator password = new("Goby1aaa");
+            password.IsCorrectLength();
+            password.HasCorrectCases();
+            password.IsDigit();
+            password.CreateShiftedString1();
+            password.CreateShiftedString2();
+            password.HasRepeatingCharacters();
+            Console.Write(password.IsPasswordValid());
         }
     }
 }
